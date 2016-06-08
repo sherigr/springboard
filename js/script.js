f852e3781b8888b1c3796044f95fb6220c5d92be
sherigr <SGrossman00@gmail.com>
2016-06-08 16:59:01 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+(function($){
+	$(function() {
+
+		
+  	
+
+  		// Fixed header
+		//-----------------------------------------------
+		$(window).scroll(function() {
+			if (($(".header.fixed").length > 0)) { 
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+					$("body").addClass("fixed-header-on");
+				} else {
+					$("body").removeClass("fixed-header-on");
+				}
+			};
+		});
+
+		$(window).load(function() {
+			if (($(".header.fixed").length > 0)) { 
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+					$("body").addClass("fixed-header-on");
+				} else {
+					$("body").removeClass("fixed-header-on");
+				}
+			};
+		});
+
+				//Scroll Spy
+		//-----------------------------------------------
+		if($(".scrollspy").length>0) {
+			$("body").addClass("scroll-spy");
+			$('body').scrollspy({ 
+				target: '.scrollspy',
+				offset: 155
+			});
+		}
+
+		// Stellar.js Div
+		//-----------------------------------------------
+		$.stellar({
+			horizontalScrolling: false
+		});
+
+
+
+		/*==========================================
+    FOOTER
+  =====================================================*/
+
+  var today = new Date();
+  var year = today.getFullYear();
+
+   $('#footer').html('<p>Springboard &copy' + year + '</p>');
+
+
+
+	});
+})(this.jQuery);
