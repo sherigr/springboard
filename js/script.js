0209d6a7323378d7aaa0f8f694b58843349229d8
sherigr <SGrossman00@gmail.com>
2016-06-22 15:02:09 -0400

    changed window width scroll and scroll spy

--- js/script.js
+++ js/script.js
@@ -6,9 +6,29 @@
 
   		// Fixed header
 		//-----------------------------------------------
+		// $(window).scroll(function() {
+		// 	if (($(".header.fixed").length > 0)) { 
+		// 		if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+		// 			$("body").addClass("fixed-header-on");
+		// 		} else {
+		// 			$("body").removeClass("fixed-header-on");
+		// 		}
+		// 	};
+		// });
+
+
+		// $(window).load(function() {
+		// 	if (($(".header.fixed").length > 0)) { 
+		// 		if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+		// 			$("body").addClass("fixed-header-on");
+		// 		} else {
+		// 			$("body").removeClass("fixed-header-on");
+		// 		}
+		// 	};
+		// });
 		$(window).scroll(function() {
 			if (($(".header.fixed").length > 0)) { 
-				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+				if(($(this).scrollTop() > 0) && ($(window).width() > 100)) {
 					$("body").addClass("fixed-header-on");
 				} else {
 					$("body").removeClass("fixed-header-on");
@@ -16,15 +36,22 @@
 			};
 		});
 
+
 		$(window).load(function() {
 			if (($(".header.fixed").length > 0)) { 
-				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+				if(($(this).scrollTop() > 0) && ($(window).width() > 100)) {
 					$("body").addClass("fixed-header-on");
 				} else {
 					$("body").removeClass("fixed-header-on");
 				}
 			};
 		});
+
+		    // COLLAPSE NAVBAR WHEN LINK IS CLICKED
+    $(".navbar-nav li a").click(function(event) {
+      $(".navbar-collapse").collapse('hide');
+    });
+
 
 				//Scroll Spy
 		//-----------------------------------------------
@@ -32,15 +59,19 @@
 			$("body").addClass("scroll-spy");
 			$('body').scrollspy({ 
 				target: '.scrollspy',
-				offset: 155
+				offset: 140
 			});
+			// $('.noSpy').css({'visibility':'hidden', 'width':'0%'});
+			$('.noSpy').css({'display':'none'});
 		}
+
 
 		// Stellar.js Div
 		//-----------------------------------------------
 		$.stellar({
 			horizontalScrolling: false,
-			hideDistantElements: true
+			hideDistantElements: true,
+			responsive: true
 		});
 
 
