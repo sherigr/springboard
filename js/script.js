eb3b14250bac24158c670c64587ce4f5224e7980
sherigr <SGrossman00@gmail.com>
2016-07-05 16:45:11 -0400

    smooth scroll

--- js/script.js
+++ js/script.js
@@ -6,28 +6,9 @@
 
   		// Fixed header
 		//-----------------------------------------------
-		// $(window).scroll(function() {
-		// 	if (($(".header.fixed").length > 0)) { 
-		// 		if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
-		// 			$("body").addClass("fixed-header-on");
-		// 		} else {
-		// 			$("body").removeClass("fixed-header-on");
-		// 		}
-		// 	};
-		// });
 
-
-		// $(window).load(function() {
-		// 	if (($(".header.fixed").length > 0)) { 
-		// 		if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
-		// 			$("body").addClass("fixed-header-on");
-		// 		} else {
-		// 			$("body").removeClass("fixed-header-on");
-		// 		}
-		// 	};
-		// });
 		$(window).scroll(function() {
-			if (($(".header.fixed").length > 0)) { 
+			if(($(".header.fixed").length > 0)) { 
 				if(($(this).scrollTop() > 0) && ($(window).width() > 100)) {
 					$("body").addClass("fixed-header-on");
 					$("img.logo-color").attr('src', 'img/springboard75.png');
@@ -37,11 +18,19 @@
 					$("img.logo-color").attr('src', 'img/springboard75white.png');
 				}
 			};
+			//parallax works but img drops on scroll
+		  // var yPos =-($(window).scrollTop() / 6);
+		  // if($(window).scrollTop() > 100) {
+		  //   document.getElementById("banner-img").style.backgroundPosition="100% "+yPos+"px";
+		  // }
+		  // if($(window).scrollTop() < 100)  {		 
+		  //   document.getElementById("banner-img").style.backgroundPosition="50% 50%";
+		  // }
 		});
 
 
 		$(window).load(function() {
-			if (($(".header.fixed").length > 0)) { 
+			if(($(".header.fixed").length > 0)) { 
 				if(($(this).scrollTop() > 0) && ($(window).width() > 100)) {
 					$("body").addClass("fixed-header-on");
 					$(".springboard-slogan").css({'display':'none'});
@@ -56,12 +45,8 @@
       $(".navbar-collapse").collapse('hide');
     });
 
-      // Scroll To Top Of Page
-    //--------------------------------------------
 
-
-
-				//Scroll Spy
+			//Scroll Spy
 		//-----------------------------------------------
 		if($(".scrollspy").length>0) {
 			$("body").addClass("scroll-spy");
@@ -69,7 +54,6 @@
 				target: '.scrollspy',
 				offset: 140
 			});
-			// $('.noSpy').css({'visibility':'hidden', 'width':'0%'});
 			$('.noSpy').css({'display':'none'});
 		}
 
@@ -79,34 +63,27 @@
 		$.stellar({
 			horizontalScrolling: false,
 			hideDistantElements: true,
-			responsive: true
+			// responsive: true
 		});
 
-
-		// $('.banner-img').stellar({
-		// 	horizontalOffset: 0,
-		//   verticalOffset: 0
-		// });
 
 	
 			//Smooth Scroll
 		//-----------------------------------------------
-		//Smooth Scroll
+		//Smooth Scroll - breaking footer
 		//-----------------------------------------------
-		if ($(".smooth-scroll").length>0) {
-			$('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function() {
-				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-					var target = $(this.hash);
-					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-					if (target.length) {
-						$('html,body').animate({
-							scrollTop: target.offset().top-151
-						}, 1000);
-						return false;
-					}
-				}
-			});
-		}
+		$('a[href*="#"]:not([href="#"])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		      var target = $(this.hash);
+		      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		      if (target.length) {
+		        $('html, body').animate({
+		          scrollTop: target.offset().top
+		        }, 1000);
+		        return false;
+		      }
+		    }
+		  });
 
 
    // FOOTER
@@ -115,7 +92,7 @@
   var today = new Date();
   var year = today.getFullYear();
 
-   $('#footer').html('<p>Springboard &copy' + year + '</p>');
+   $('#footer').html('<p>&copy ' + year + ' Springboard</p>');
 
 
 
